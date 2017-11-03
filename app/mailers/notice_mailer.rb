f894c4370d14c5a0aeb0fa5109edebddc89fb099
伊藤淳 <itojun@itojunMacBook-Pro.local>
2017-11-03 10:47:21 +0900

    DIVE20_exam update_20171103

--- app/mailers/notice_mailer.rb
+++ app/mailers/notice_mailer.rb
@@ -9,7 +9,7 @@ class NoticeMailer < ApplicationMailer
     @greeting = "Hi"
     @topic = topic
 
-    mail to: "test1@test.com",
+    mail to: @topic.user.email,
          subject: '【なんちゃってFacebook】新規記事が投稿されました'
   end
 end
